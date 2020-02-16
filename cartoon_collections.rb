a03a391ae033b36f35277eb226b1ed775225953f
Samuel Escobar <sescobar8714@gmail.com>
2020-02-16 22:47:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ def roll_call_dwarves(array)# code an argument here
   end
 end
 
-roll_call_dwarves(dwarves)
+roll_call_dwarves(array)
 
 def summon_captain_planet# code an argument here
   # Your code here
