ad4c2d3fd266f67d0360b7ccb860a4bd12d26626
Samuel Escobar <sescobar8714@gmail.com>
2020-02-16 23:13:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,10 +24,10 @@ def long_planeteer_calls(array)
 i = 0
 if array.any? {|i| i.length > 4}
   return true
-else 
+else
   return false
   i = i + 1
-end 
+end
   # code an argument here
   # Your code here
 end
